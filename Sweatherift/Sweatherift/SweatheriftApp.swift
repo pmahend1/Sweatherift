4bc175254b2c75fc9f3244efc296cb08d1ded68a
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2023-04-27 16:14:30 -0400

    added SwiftUI project

---
+++ Sweatherift/Sweatherift/SweatheriftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweatheriftApp.swift
+//  Sweatherift
+//
+//  Created by Prateek Mahendrakar on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SweatheriftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
