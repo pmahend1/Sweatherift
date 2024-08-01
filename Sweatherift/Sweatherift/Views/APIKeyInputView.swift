1d5e4883e02fd39af2c362007e3aaa3422a6d9b9
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2024-08-01 00:41:30 -0400

    Deployment target changed to 17.5 Reformatting

--- Sweatherift/Sweatherift/Views/APIKeyInputView.swift
+++ Sweatherift/Sweatherift/Views/APIKeyInputView.swift
@@ -8,67 +8,68 @@
 import SwiftUI
 
 struct APIKeyInputView: View {
-   @StateObject var viewModel = APIKeyInputViewModel()
+    @StateObject var viewModel = APIKeyInputViewModel()
 
-   var body: some View {
-      Text(.init(Localized.registerAtHttpsOpenweathermapOrg))
-      Text(.init(Localized.goToHttpsHomeOpenweathermapOrgApiKeysToGetAKey))
-         .padding(.top, 10)
+    var body: some View {
+        Text(.init(Localized.registerAtHttpsOpenweathermapOrg))
+        Text(.init(Localized.goToHttpsHomeOpenweathermapOrgApiKeysToGetAKey))
+            .padding(.top, 10)
 
-      Text(Localized.currentAPIKey)
-         .font(.caption2)
-         .padding(.top, 20)
+        Text(Localized.currentAPIKey)
+            .font(.caption2)
+            .padding(.top, 20)
 
-      if viewModel.isKeyPresent {
-         Text(viewModel.APIKey)
-            .redacted(reason: !viewModel.showAPIKey ? .placeholder : [])
-            .font(.body)
-            .overlay {
-               Text(Localized.show)
-                  .opacity(viewModel.showAPIKey ? 0 : 1)
-                  .font(.caption2)
+        if viewModel.isKeyPresent {
+            Text(viewModel.APIKey)
+                .redacted(reason: !viewModel.showAPIKey ? .placeholder : [])
+                .font(.body)
+                .overlay {
+                    Text(Localized.show)
+                        .opacity(viewModel.showAPIKey ? 0 : 1)
+                        .font(.caption2)
+                }
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    viewModel.showAPIKey.toggle()
+                }
+        }
+
+        VStack(alignment: .center, spacing: .zero) {
+            VStack(alignment: .leading, spacing: 4) {
+                Text(Localized.key)
+                    .font(.caption)
+                    .padding(.leading, 2)
+
+                TextField(Localized.enterKey, text: $viewModel.text)
+                    .textFieldStyle(.roundedBorder)
+                    .autocorrectionDisabled()
+
+                Button(viewModel.isKeyPresent ? Localized.change : Localized.save) {
+                    _ = viewModel.save()
+                }
+                .font(.body.bold())
+                .frame(width: 200, height: 40)
+                .background(Color.accent)
+                .foregroundColor(Color.buttonLabelColor)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(.top, 10)
+
+                if viewModel.showKeyUpdatedMessage {
+                    Text(Localized.apiKeyHasBeenUpdated)
+                        .font(.caption)
+                        .padding(.top, 10)
+                }
             }
-            .contentShape(Rectangle())
-            .onTapGesture {
-               viewModel.showAPIKey.toggle()
+            .padding(.all, 20)
+            .onAppear {
+                viewModel.loadData()
             }
-      }
-
-      VStack(alignment: .center, spacing: .zero) {
-         VStack(alignment: .leading, spacing: 4) {
-            Text(Localized.key)
-               .font(.caption)
-               .padding(.leading, 2)
-
-            TextField(Localized.enterKey, text: $viewModel.text)
-               .textFieldStyle(.roundedBorder)
-         }
-
-         Button(viewModel.isKeyPresent ? Localized.change : Localized.save) {
-            _ = viewModel.save()
-         }
-         .font(.body.bold())
-         .frame(width: 200, height: 40)
-         .background(Color.accent)
-         .foregroundColor(Color.buttonLabelColor)
-         .clipShape(RoundedRectangle(cornerRadius: 10))
-         .padding(.top, 10)
-
-         if viewModel.showKeyUpdatedMessage {
-            Text(Localized.apiKeyHasBeenUpdated)
-               .font(.caption)
-               .padding(.top, 10)
-         }
-      }
-      .padding(.all, 20)
-      .onAppear {
-         viewModel.loadData()
-      }
-   }
+        }
+    }
 }
 
 struct APIKeyInputView_Previews: PreviewProvider {
-   static var previews: some View {
-      APIKeyInputView()
-   }
+    static var previews: some View {
+        APIKeyInputView()
+    }
 }
